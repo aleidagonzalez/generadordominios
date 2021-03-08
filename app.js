4576f7c5b6b42adae01c5211a88011207b18ac3b
aleidagonzalez <aleida1983@hotmail.com>
2021-03-08 17:47:11 +0000

    dominios

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+var dom = [".com", ".es"];
+for (var i = 0; i < pronoun.length; i++) {
+  for (var a = 0; a < adj.length; a++) {
+    for (var n = 0; n < noun.length; n++) {
+      for (var d = 0; d < dom.length; d++) {
+        console.log(pronoun[i].concat(adj[a], noun[n], dom[d]));
+      }
+    }
+  }
+}
